1602f8702af7c3638d42d7b3572b4149cd5ddc79
Lloyd Francis <theapologist@Lloyds-MacBook-Pro.local>
2019-03-16 20:13:56 +0530

    bug fix, changing promise resolution callback argument order

--- src/mongo/connect.js
+++ src/mongo/connect.js
@@ -1,7 +1,7 @@
 const MongoClient = require('mongodb').MongoClient
 const { clientUrl, db } = require('../constants/environment')
 const connect = () => {
-  return MongoClient.connect(clientUrl).then((err, client) => {
+  return MongoClient.connect(clientUrl).then((client, err) => {
     if (err) throw err
     return client.db(db)
   })
